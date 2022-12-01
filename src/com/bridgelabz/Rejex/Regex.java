4dfa2b5a6a05b60e135c78bce89bbbfea95b71ec
surabhisanjan <surabhisanjan@gmail.com>
2022-12-01 14:33:10 +0530

    Updation: Ability to validate Last name

--- src/com/bridgelabz/Rejex/Regex.java
+++ src/com/bridgelabz/Rejex/Regex.java
@@ -2,24 +2,25 @@ package com.bridgelabz.Rejex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Regex {
-        public static boolean validate(String fName) {
+        public static boolean validate(String LName) {
             String firstName= "^[A-Z]{1}[A-Za-z]{2,}$";
             Pattern obj1= Pattern.compile(firstName);
-            if(fName==null) {
+            if(LName==null) {
                 return false;
             }
-            Matcher obj2 = obj1.matcher(fName);
+            Matcher obj2 = obj1.matcher(LName);
             return obj2.matches();
         }
         public static void main(String[] args) {
 
             System.out.println("Check Name Validation");
             boolean fN = validate("Surabhi");
+            boolean lN= validate("Sanjan");
 
-           if(fN)
-              System.out.println("Surabhi is a Valid firstName");
+           if(lN)
+              System.out.println("Sanjan is a Valid LastName");
             else
-                System.out.println("Surabhi is an Invalid firstName");
+                System.out.println("Sanjan is an Invalid LastName");
         }
 
 
